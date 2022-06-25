b2d52bb5b13eecd0b1e0c47c6f52298e6927a325
Zach Posten <zach@posten.io>
2022-06-25 02:20:39 -0500

    chore: Install & configure tailwind CSS
    Very helpful instructions:
    https://tailwindcss.com/docs/guides/sveltekit

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -21,11 +21,19 @@
   <title>Home</title>
 </svelte:head>
 
-<button on:click={sendMessage}>Send message</button>
+<button
+  on:click={sendMessage}
+  class="rounded p-4 bg-blue-600 m-4 hover:bg-blue-700 active:bg-blue-800 text-white foo"
+  >Send message</button
+>
 
 <style>
   :global(body) {
     background: #2b2b2b;
     color: white;
+  }
+
+  .foo {
+    border: 2px solid rebeccapurple;
   }
 </style>
