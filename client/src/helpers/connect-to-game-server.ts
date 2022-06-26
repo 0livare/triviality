78f1d3117bbeb21e1b1a835aaa5df6627de182f4
Zach Posten <zach@posten.io>
2022-06-26 11:16:28 -0500

    fix: Change server url to Digital Ocean

--- client/src/helpers/connect-to-game-server.ts
+++ client/src/helpers/connect-to-game-server.ts
@@ -1,5 +1,13 @@
 import { io } from 'socket.io-client'
 
 export function connect() {
-  return io('ws://localhost:3001')
+  // return io('ws://localhost:3001')
+
+  // return io('http://147.182.246.145')
+  return io('https://olivare.net')
+
+  // See: https://socket.io/docs/v3/troubleshooting-connection-issues/
+  // return io('http://147.182.246.145', {
+  //   rejectUnauthorized: false, // WARN: please do not do this in production
+  // })
 }
