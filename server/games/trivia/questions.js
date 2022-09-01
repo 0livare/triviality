3df0f565b3a7c0411eb04c4be3d59e5936875e91
Zach Posten <zach@posten.io>
2022-08-31 22:49:15 -0500

    chore: Add real marriage questions

--- server/games/trivia/questions.js
+++ server/games/trivia/questions.js
@@ -30,4 +30,102 @@ module.exports = [
     ],
     answer: 'All of the above',
   },
+  {
+    prompt: 'Where does the nickname Kiermo Bean come from?',
+    choices: ['', '', '', ''],
+    answer: 'Seth in Taco Bell',
+  },
+  {
+    prompt: 'What were the first sports we played?',
+    choices: ['', '', '', ''],
+    answer: 'Baseball and soccer',
+  },
+  {
+    prompt: "What is Zach's nickname for his sister?",
+    choices: ['Butter', 'Nikki', 'Smidge', 'Little'],
+    answer: 'Smidge',
+  },
+  {
+    prompt: "Who was Kierstyn's first high school boyfriend?",
+    choices: ['', '', '', ''],
+    answer: 'Josh',
+  },
+  {
+    prompt: 'What instrument did Zach play in band?',
+    choices: ['', '', '', ''],
+    answer: 'Trumpet',
+  },
+  {
+    prompt: "What was Kierstyn's original college major?",
+    choices: ['', '', '', ''],
+    answer: '',
+  },
+  {
+    prompt: 'What street did Zach and Scott live on in college?',
+    choices: ['', '', '', ''],
+    answer: 'Water',
+  },
+  {
+    prompt: 'Who does Kierstyn have a matching ribcage tattoo with?',
+    choices: ['Britni', 'Sierra', 'Erica', 'Kaia'],
+    answer: '',
+  },
+  {
+    prompt: 'How many baseball stadiums has Zach been to?',
+    choices: ['', '', '', ''],
+    answer: '',
+  },
+  {
+    prompt: 'Where did Zach and Kierstyn meet?',
+    choices: [
+      'Tinder',
+      'Work',
+      'Bumble',
+      'The grocery store',
+      'A bar',
+      'A baseball game',
+      'School',
+    ],
+    answer: 'Work',
+  },
+  {
+    prompt: 'What product did Zach and Kierstyn work on together?',
+    choices: ['GLAS', 'Metasys', 'NS 8000', 'Bright Cellars'],
+    answer: 'GLAS',
+  },
+  {
+    prompt: "Which of Zach and Kierstyn's friends can hang?",
+    choices: ['Jon', 'Scott', 'Sierra', 'NOT Matt'],
+    answer: 'NOT Matt',
+  },
+  {
+    prompt: 'How many states have Zach and Kierstyn celebrated Thanksgiving in?',
+    choices: ['One', 'Two', 'Three', 'Four'],
+    answer: 'Three',
+  },
+  {
+    prompt: "What is Kierstyn's favorite Milwaukee bar?",
+    choices: ['Brothers', 'Boone and Crockett', '1840', 'Sugar Maple'],
+    answer: 'Boone and Crockett',
+  },
+  {
+    prompt: "What was Kierstyn's first tattoo?",
+    choices: ['', '', '', ''],
+    answer: '',
+  },
+  {
+    prompt: 'How many tattoos does Kierstyn have?',
+    choices: ['Six', 'Ten', 'Fourteen', 'Sixteen'],
+    answer: '',
+  },
+  {
+    prompt: "In school, what was Zach's favorite and Kierstyn's least favorite subject?",
+    choices: ['Gym', 'Recess', 'Science', 'Math'],
+    answer: 'Math',
+  },
+  // {
+  //   prompt: 'What last name will Zach & Kierstyn Share?',
+  //   choices: ['Olivíar', 'Olivare', 'Ahlivare', 'Olivair'],
+  //   answer: 'Olivare',
+  // },
 ]
