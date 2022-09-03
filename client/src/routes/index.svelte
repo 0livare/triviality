32de0e53e53f7874c03c2b0503033c2db3e35701
Zach Posten <zach@posten.io>
2022-09-03 08:57:14 -0500

    chore: Redirect "/" to "/trivia"

--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -1,6 +1,12 @@
-Choose a game:
+<script lang="ts">
+  import { goto } from '$app/navigation'
+
+  goto('/trivia')
+</script>
+
+<!-- Choose a game:
 
 <ul>
   <li><a href="/trivia">Wisconsin Trivia</a></li>
   <li><a href="/buzz">Buzz</a></li>
-</ul>
+</ul> -->
